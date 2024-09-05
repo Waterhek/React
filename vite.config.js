af89f76de008c3f3ddc2d9b465a4096b69605d34
Waterhek <zazori425@gmail.com>
2024-09-06 04:26:02 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 import vercel from 'vite-plugin-vercel';
 
 export default defineConfig({
+    build: {
+        outDir: 'public',
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.jsx'],
