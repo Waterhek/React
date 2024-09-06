8a66901e54f93332ccdff3277180ad10d7e71b04
Waterhek <zazori425@gmail.com>
2024-09-06 14:42:52 +0800

    fix error

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    build: {
-        outDir: 'public',
-    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.jsx'],
