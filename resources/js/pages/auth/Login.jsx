93721a77cd2e3f752a7666a8510e472f96f2858a
Waterhek <zazori425@gmail.com>
2024-09-07 03:58:04 +0800

    daily update

--- resources/js/pages/auth/Login.jsx
+++ resources/js/pages/auth/Login.jsx
@@ -1,7 +1,21 @@
+import '../../../css/login.css'
+
 export default function login(){
     return(
         <div>
-            Login
+            <form>
+                <div class="login-container">
+                    <h1 class="form-title">Login</h1>
+                    <div class="form-section">
+                        <label class="form-label">Username</label>
+                        <input class="form-control" type="text" name="username" />
+                    </div>
+                    <div class="form-section">
+                        <label class="form-label">Password</label>
+                        <input class="form-control" type="password" name="password" />
+                    </div>
+                </div>
+            </form>
         </div>
     )
 }
