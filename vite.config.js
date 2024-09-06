54a70406e99d1d15eb349beaf082ba88d73827b2
Waterhek <zazori425@gmail.com>
2024-09-06 14:26:41 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
-import vercel from 'vite-plugin-vercel';
 
 export default defineConfig({
     build: {
